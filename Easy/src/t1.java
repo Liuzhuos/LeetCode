bf1e71b07d6abbdb6da353e1ed7045613491a6d1
LiuTZ <2725926487@qq.com>
2023-10-16 20:06:24 +0800

    first commit

---
+++ Easy/src/t1.java
@@ -0,0 +1,16 @@
+/*
+ *太原学院计科系 拥有本软件版权2021，并保留所有权限、
+ *Copyright 2021，YCHS Iformation&science Technology Co.,Ltd,
+ *All rights reserved.
+ */
+
+/**
+ * @author liuzhuo
+ * @version 1.0
+ * @date 2023/10/16
+ */
+public class t1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
