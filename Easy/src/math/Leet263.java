111b33469013fe04c4da0e9a934f3ff2db3c5cee
LiuTZ <2725926487@qq.com>
2023-10-24 23:03:16 +0800

    LeetCode263丑数

---
+++ Easy/src/math/Leet263.java
@@ -0,0 +1,28 @@
+/*
+ *太原学院计科系 拥有本软件版权2021，并保留所有权限、
+ *Copyright 2021，YCHS Iformation&science Technology Co.,Ltd,
+ *All rights reserved.
+ */
+package math;
+
+/**
+ * @author liuzhuo
+ * @version 1.0
+ * @date 2023/10/24
+ */
+public class Leet263 {
+    public boolean isUgly(int n) {
+        if(n==0) return false;
+        while(n%2==0){
+            n = n/2;
+        }
+        while(n%3==0){
+            n = n/3;
+        }
+        while(n%5==0){
+            n = n/5;
+        }
+        if(n==1) return true;
+        else return false;
+    }
+}
