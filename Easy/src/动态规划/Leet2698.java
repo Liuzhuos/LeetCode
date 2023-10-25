85a9348d6064401f92abd0e3e7c06c9b1813146f
LiuTZ <2725926487@qq.com>
2023-10-25 10:31:26 +0800

    LeetCode2698求一个整数的惩罚数

---
+++ Easy/src/动态规划/Leet2698.java
@@ -0,0 +1,41 @@
+/*
+ *太原学院计科系 拥有本软件版权2021，并保留所有权限、
+ *Copyright 2021，YCHS Iformation&science Technology Co.,Ltd,
+ *All rights reserved.
+ */
+package 动态规划;
+
+/**
+ * @author liuzhuo
+ * @version 1.0
+ * @date 2023/10/25
+ */
+public class Leet2698 {
+    public int punishmentNumber(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            String s = Integer.toString(i * i);
+            if (dfs(s, 0, 0, i)) {
+                res += i * i;
+            }
+        }
+        return res;
+    }
+
+    public boolean dfs(String s, int pos, int tot, int target) {
+        if (pos == s.length()) {
+            return tot == target;
+        }
+        int sum = 0;
+        for (int i = pos; i < s.length(); i++) {
+            sum = sum * 10 + s.charAt(i) - '0';
+            if (sum + tot > target) {
+                break;
+            }
+            if (dfs(s, i + 1, sum + tot, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
