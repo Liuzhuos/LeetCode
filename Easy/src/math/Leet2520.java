a62c861728a9ea69d18813fb2cecf335f3895947
LiuTZ <2725926487@qq.com>
2023-10-26 09:05:35 +0800

    LeetCode2520统计能整除数字的位数

---
+++ Easy/src/math/Leet2520.java
@@ -0,0 +1,25 @@
+/*
+ *太原学院计科系 拥有本软件版权2021，并保留所有权限、
+ *Copyright 2021，YCHS Iformation&science Technology Co.,Ltd,
+ *All rights reserved.
+ */
+package math;
+
+/**
+ * @author liuzhuo
+ * @version 1.0
+ * @date 2023/10/26
+ */
+public class Leet2520 {
+    public int countDigits(int num) {
+        int res = 0;
+        int n = num;
+        String s = num + "";
+        for (int i = 0; i < s.length(); i++) {
+            int a = num%10;
+            num = num/10;
+            if(n%a==0) res++;
+        }
+        return res;
+    }
+}
