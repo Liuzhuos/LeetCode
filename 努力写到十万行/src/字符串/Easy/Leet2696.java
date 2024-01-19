71954546cf023adc0ef6da078458ec6f61980a4f
LiuTZ <2725926487@qq.com>
2024-01-19 11:57:20 +0800

    Leet2696

---
+++ 努力写到十万行/src/字符串/Easy/Leet2696.java
@@ -0,0 +1,21 @@
+/*
+ *太原学院计科系 拥有本软件版权2021，并保留所有权限、
+ *Copyright 2021，YCHS Iformation&science Technology Co.,Ltd,
+ *All rights reserved.
+ */
+package 字符串.Easy;
+
+/**
+ * @author liuzhuo
+ * @version 1.0
+ * @date 2024/1/19
+ */
+public class Leet2696 {
+    public int minLength(String s) {
+        while (s.contains("AB") || s.contains("CD")) {
+            s = s.replace("AB", "");
+            s = s.replace("CD", "");
+        }
+        return s.length();
+    }
+}
