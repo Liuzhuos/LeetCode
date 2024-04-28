f8f037ed34f29a4284c97826acb84b31781b4fad
LiuTZ <2725926487@qq.com>
2024-04-28 21:27:21 +0800

    Leet1017

---
+++ 努力写到十万行/src/math/Middle/Leet1017.java
@@ -0,0 +1,27 @@
+/*
+ *太原学院计科系 拥有本软件版权2021，并保留所有权限、
+ *Copyright 2021，YCHS Iformation&science Technology Co.,Ltd,
+ *All rights reserved.
+ */
+package math.Middle;
+
+/**
+ * @author liuzhuo
+ * @version 1.0
+ * @date 2024/4/28
+ */
+public class Leet1017 {
+    public String baseNeg2(int n) {
+        if (n == 0 || n == 1) {
+            return String.valueOf(n);
+        }
+        StringBuilder res = new StringBuilder();
+        while (n != 0) {
+            int remainder = n & 1;
+            res.append(remainder);
+            n -= remainder;
+            n /= -2;
+        }
+        return res.reverse().toString();
+    }
+}
