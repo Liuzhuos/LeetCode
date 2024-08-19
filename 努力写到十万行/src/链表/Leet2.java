abe9b917d9fb0f6ad2c829c0d1ed1885a98639d7
LiuTZ <2725926487@qq.com>
2024-08-19 17:21:54 +0800

    Leet

---
+++ 努力写到十万行/src/链表/Leet2.java
@@ -0,0 +1,57 @@
+/*
+ *太原学院计科系 拥有本软件版权2021，并保留所有权限、
+ *Copyright 2021，YCHS Iformation&science Technology Co.,Ltd,
+ *All rights reserved.
+ */
+package 链表;
+
+/**
+ * @author liuzhuo
+ * @version 1.0
+ * @date 2024/8/19
+ */
+public class Leet2 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
