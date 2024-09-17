9a660448aac646da9da49af0b588dd045e550a65
LiuTZ <2725926487@qq.com>
2024-09-17 23:14:07 +0800

    图计算—攻破

---
+++ 努力写到十万行/src/图/Diff/Leet815.java
@@ -0,0 +1,59 @@
+/*
+ *太原学院计科系 拥有本软件版权2021，并保留所有权限、
+ *Copyright 2021，YCHS Iformation&science Technology Co.,Ltd,
+ *All rights reserved.
+ */
+package 图.Diff;
+
+import java.util.*;
+
+/**
+ * @author liuzhuo
+ * @version 1.0
+ * @date 2024/9/17
+ */
+public class Leet815 {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) {
+            return 0;
+        }
+        int n = routes.length;
+        boolean[][] edge = new boolean[n][n];
+        Map<Integer, List<Integer>> rec = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int site : routes[i]) {
+                List<Integer> list = rec.getOrDefault(site, new ArrayList<Integer>());
+                for (int j : list) {
+                    edge[i][j] = edge[j][i] = true;
+                }
+                list.add(i);
+                rec.put(site, list);
+            }
+        }
+
+        int[] dis = new int[n];
+        Arrays.fill(dis, -1);
+        Queue<Integer> que = new LinkedList<>();
+        for (int bus : rec.getOrDefault(source, new ArrayList<Integer>())) {
+            dis[bus] = 1;
+            que.offer(bus);
+        }
+        while (!que.isEmpty()) {
+            int x = que.poll();
+            for (int y = 0; y < n; y++) {
+                if (edge[x][y] && dis[y] == -1) {
+                    dis[y] = dis[x] + 1;
+                    que.offer(y);
+                }
+            }
+        }
+
+        int ret = Integer.MAX_VALUE;
+        for (int bus : rec.getOrDefault(target, new ArrayList<Integer>())) {
+            if (dis[bus] != -1) {
+                ret = Math.min(ret, dis[bus]);
+            }
+        }
+        return ret == Integer.MAX_VALUE ? -1 : ret;
+    }
+}
